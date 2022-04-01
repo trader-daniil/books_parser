df18c31fd7b88b8b558ccc9c327ca049b33dcbdb
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-04-01 11:08:39 +0300

    download books with titles

--- parse_library.py
+++ parse_library.py
@@ -1,15 +1,44 @@
 import os
 from pathlib import Path
+from pathvalidate import sanitize_filename
 
 import requests
+from bs4 import BeautifulSoup
 
 
+def check_redirect(book_response):
+    if book_response.history:
+        requests.exceptions.HTTPError
+
+
+def get_book_soup(book_link):
+    book_response = requests.get(url=book_link)
+    book_response.raise_for_status()
+    soup = BeautifulSoup(book_response.text, 'lxml')
+    return soup
+
+
+def get_book_name(booksoup):
+    bookname = booksoup.find('body').find('table', class_='tabs').find('h1').text
+    bookname = bookname.split('::')[0].strip()
+    bookname = sanitize_filename(bookname)
+    return bookname
+
+
+def get_book_image(booksoup):
+    image_path = booksoup.find('table', class_='d_book').find('img')['src']
+    full_image_link = f'https://tululu.org{image_path}'
+    return full_image_link
+
+
+def check_redirect(book_response):
+    if book_response.history:
+        raise requests.HTTPError
 
 
 def download_book(book_link, bookname):
-    book_response = requests.get(
-        url=book_link,
-    )
+    book_response = requests.get(url=book_link)
+    check_redirect(book_response)
     book_response.raise_for_status()
     book_path = f'books/{bookname}.txt'
     with open(book_path, 'wb') as file:
@@ -17,55 +46,27 @@ def download_book(book_link, bookname):
     
 
 
-def get_full_image_path(image_url, image_folder):
-    image_name =  os.path.basename(image_url)
-    full_image_path = f'{image_folder}/{image_name}'
-    return full_image_path
-
-
-
-def download_image(image_url, image_path):
-    image_response = requests.get(
-        url=image_url,
-    )
-    image_response.raise_for_status()
-    with open(image_path, 'wb') as file:
-        file.write(image_response.content)
-
-
 photos_path = 'books'
 Path(photos_path).mkdir(
     parents=True,
     exist_ok=True,
 )
+
 
 for book_id in range(1, 11):
-    book_url = f'https://tululu.org/txt.php?id={book_id}'
-    download_book(
-        book_link=book_url,
-        bookname=f'book{book_id}',
-    )
+    book_url = f'https://tululu.org/b{book_id}/'
+    downloading_book_url = f'https://tululu.org/txt.php?id={book_id}'
+    booksoup = get_book_soup(book_link=book_url)
+    bookname = get_book_name(booksoup=booksoup)
+    try:
+        download_book(
+            book_link=downloading_book_url,
+            bookname=bookname,
+        )
+    except requests.exceptions.HTTPError:
+        continue
 
+    
 
+    
 
-"""
-photos_path = 'images'
-Path(photos_path).mkdir(
-    parents=True,
-    exist_ok=True,
-)
-photos_path = 'books'
-Path(photos_path).mkdir(
-    parents=True,
-    exist_ok=True,
-)
-url = 'https://dvmn.org/media/Requests_Python_Logo.png'
-images_path = get_full_image_path(
-    image_url=url,
-    image_folder=photos_path,
-)
-download_image(
-    image_url=url,
-    image_path=images_path,
-)
-"""
