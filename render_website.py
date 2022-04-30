b45c17751f1ba17183e4bfdaa6f314ed063edfff
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-04-30 21:08:11 +0300

    correct liverload

--- render_website.py
+++ render_website.py
@@ -7,30 +7,13 @@ from livereload import Server
 from more_itertools import chunked
 
 
-def on_reload(template, data, filename, pages_amount, page_num):
-    next_page = math.ceil(page_num) + 1
-    prev_page = math.ceil(page_num) - 1
-    pages_info = {
-        'pages_amount': pages_amount,
-        'next_page': next_page,
-        'prev_page': prev_page,
-        'page_num': page_num,
-    }
-    rendered_page = template.render(
-        books_info=data,
-        pages_info=pages_info,
-    )
-    with open(filename, 'w', encoding="utf8") as file:
-        file.write(rendered_page)
-
-
-def main():
-    books_per_page = 10
+def on_reload():
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml']),
     )
     template = env.get_template('index.html')
+    books_per_page = 10
     with open(
         "data_books/books_info.json",
         "r",
@@ -38,24 +21,33 @@ def main():
     ) as write_file:
         books_on_page = json.load(write_file)
     books_on_page = list(chunked(books_on_page, books_per_page))
+    for books_num, books in enumerate(books_on_page, start=1):
+        file_with_books_path = f'pages/page_with_books{books_num}.html'
+        books_in_block = list(chunked(books, 2))
+        next_page = math.ceil(books_num) + 1
+        prev_page = math.ceil(books_num) - 1
+        pages_info = {
+            'pages_amount': len(books_on_page) + 1,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'page_num': books_num,
+        }
+        rendered_page = template.render(
+            books_info=books_in_block,
+            pages_info=pages_info,
+        )
+        with open(file_with_books_path, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
+
+
+def main():
     pages_path = 'pages'
     Path(pages_path).mkdir(
         parents=True,
         exist_ok=True,
     )
     server = Server()
-    for books_num, books in enumerate(books_on_page, start=1):
-        file_with_books_path = f'pages/page_with_books{books_num}.html'
-        books_in_block = list(chunked(books, 2))
-        on_reload(
-            page_num=books_num,
-            pages_amount=len(books_on_page) + 1,
-            template=template,
-            data=books_in_block,
-            filename=file_with_books_path,
-        )
-
-    server.watch('pages/*.html')
+    server.watch('pages/', on_reload)
     server.serve(root='.')
 
 
